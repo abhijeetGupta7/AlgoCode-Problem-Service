c2a2ee461b47487e0be063794c82cdb6c435adf7
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-03-27 06:37:05 +0530

    Added status codes for not implemented controllers

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,3 +1,5 @@
+const statusCodes=require("http-status-codes");
+
 function pingProblemController(req,res) {
     return res.json({
         "message":"Ping controller is up"
@@ -5,23 +7,33 @@ function pingProblemController(req,res) {
 }
 
 function addProblem(req,res) {
-
+    return res.status(501).json({   
+        message:"Not implemented"
+    });
 }
 
 function getProblem(req,res) {
-
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function getProblems(req,res) {
-
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function deleteProblem(req,res) {
-
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function updateProblem(req,res) {
-
+    return res.status(statusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 module.exports={
@@ -33,3 +45,8 @@ module.exports={
     pingProblemController
 };
 
+
+
+// res.status(...) : it returns the response object with the HTTP status code set as given
+// res.json(...) :   it returns the response object with the json to be returned is set as given
+// so that's why res.status().json() works
