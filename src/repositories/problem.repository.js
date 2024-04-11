60df57626e3fdb069f5676d3a5070e3dc57b27da
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-04-12 00:43:40 +0530

    Feat: Added API for Delete and Update Problem by its id

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -40,6 +40,31 @@ class ProblemRepository {
             throw error;
         }
     }
+
+
+    async deleteProblem(id) {
+        try {
+            const deletedProblem=await Problem.findByIdAndDelete(id);
+            if(!deletedProblem) {    
+                throw new NotFoundError("Problem",id);
+            }
+            return deletedProblem;
+        } catch(error) {
+            throw error;
+        }
+    }
+
+    async updateProblem(id,problemData) {
+        try {
+            const updatedProblem=await Problem.findByIdAndUpdate(id,problemData,{new:true});
+            if(!updatedProblem) {
+                throw new NotFoundError("Problem",id);
+            }           
+            return updatedProblem;
+        } catch(error) {
+            throw error;
+        }
+    }
 }
 
 module.exports=ProblemRepository;
